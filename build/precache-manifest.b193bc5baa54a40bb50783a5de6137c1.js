10e75548b3d9d7a533f449cbd657bc1762270e2e
asep jumadi <asepjumadi27@gmail.com>
2019-12-25 00:44:09 +0700

    update new things

---
+++ build/precache-manifest.b193bc5baa54a40bb50783a5de6137c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3188c7f4ae146123dcf400514f3d6029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c51b6ec2903b008209d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "02e5411618cced25c74a",
+    "url": "/static/css/main.f6636bf5.chunk.css"
+  },
+  {
+    "revision": "6c51b6ec2903b008209d",
+    "url": "/static/js/2.fa619891.chunk.js"
+  },
+  {
+    "revision": "aec00536e0cd6e0bfe78216689a49546",
+    "url": "/static/js/2.fa619891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02e5411618cced25c74a",
+    "url": "/static/js/main.b1f4fbfe.chunk.js"
+  },
+  {
+    "revision": "008b1b3a0a50713aea3b",
+    "url": "/static/js/runtime-main.8dddb12d.js"
+  },
+  {
+    "revision": "5ddf4059f3254ce7878276cd06297756",
+    "url": "/static/media/mini2.5ddf4059.png"
+  }
+]);
